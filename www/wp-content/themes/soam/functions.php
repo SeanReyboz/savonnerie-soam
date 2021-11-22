1f81fae68fb8adc4e4e022f4eb96eb5854c3f037
Léo Achard <leo.achard@hotmail.fr>
2021-11-22 17:05:32 +0100

    Update Font

--- www/wp-content/themes/soam/functions.php
+++ www/wp-content/themes/soam/functions.php
@@ -1,6 +1,6 @@
 <?php
 function add_style() {
-    wp_enqueue_style('main-style', get_template_directory_uri() . '/style.css', false);
+    //wp_enqueue_style('main-style', get_template_directory_uri() . '/style.css', false);
     wp_enqueue_style('main-css', get_template_directory_uri() . '/css/main.css?t='.time(), false);
   }
   add_action( 'wp_enqueue_scripts', 'add_style' );
