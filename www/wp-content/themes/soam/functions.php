63fc396a4b9f0172ae6c9e16dd5f949fa7ca955e
Léo Achard <leo.achard@hotmail.fr>
2021-11-22 15:53:12 +0100

    Update functions.php

--- www/wp-content/themes/soam/functions.php
+++ www/wp-content/themes/soam/functions.php
@@ -1,7 +1,7 @@
 <?php
 function add_style() {
     wp_enqueue_style('main-style', get_template_directory_uri() . '/style.css', false);
-    wp_enqueue_style('main-css', get_template_directory_uri() . '/css/main.min.css?t='.time(), false);
+    wp_enqueue_style('main-css', get_template_directory_uri() . '/css/main.css?t='.time(), false);
   }
   add_action( 'wp_enqueue_scripts', 'add_style' );
 
