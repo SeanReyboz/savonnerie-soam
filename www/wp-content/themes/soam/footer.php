7f860df85aadcde84802a4b8d654960442ae51c1
SeanReyboz <seanreyboz@protonmail.com>
2021-11-22 17:47:34 +0100

    added footer

--- www/wp-content/themes/soam/footer.php
+++ www/wp-content/themes/soam/footer.php
@@ -1,5 +1,18 @@
-<footer>
-    <?php dynamic_sidebar( 'footer-sidebar' ); ?>
+  <footer>
+    <div class="content">
+      <div class="brand-information">
+        <img class="brand-logo" src="<?php echo get_template_directory_uri() . '/images/logo_white.png'  ?>" alt="Logo Soâm">
+        <p class="text-sm brand-desc">
+          Une savonnerie artisanale et organique située dans les Bauges qui prend
+          soin de votre corps et de la nature. Tous mes savons et cosmétiques sont
+          100% locaux et faits mains avec des produits organiques de qualité.
+        </p>
+      </div>
+      <?php dynamic_sidebar( 'footer-sidebar' ); ?>
+    </div>
+    <small class="copyrights">
+      &copy; 2021 La Savonnerie Soâm | Design et Développement par Pikoom 
+    </small>
   </footer>
 
   <?php wp_footer(); ?>
