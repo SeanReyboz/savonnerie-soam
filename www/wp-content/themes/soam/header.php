cdb3d920f0d9451704aa340fa2961c7f730a92bd
SeanReyboz <seanreyboz@protonmail.com>
2021-11-22 17:22:45 +0100

    Now using wp function to get images

--- www/wp-content/themes/soam/header.php
+++ www/wp-content/themes/soam/header.php
@@ -2,13 +2,14 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <meta name="author" content="Agence Pikoom">
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
   </head>
   <body>
     <header>
       <div class="main_nav flex-row">
-        <a href=""><img class="logo" src="wp-content/themes/soam/images/logo.png" alt="Logo Soam"/></a>
+        <a href=""><img class="logo" src="<?php echo get_template_directory_uri() . '/images/logo.png'; ?>" alt="Logo Soam"/></a>
         
         <ul class="nav flex-row">
           <li><a class="text" href="">Gamme Soam</a></li>
@@ -18,8 +19,8 @@
           <li><a class="text"  href="">Points de ventes</a></li>
         </ul>
         <div class="rs_icons">
-          <a id="facebook" target="_blank" href="https://www.facebook.com/SoAmsavonneriebbio"  ><img src="wp-content/themes/soam/images/icons/facebook.svg" alt="Logo facebook"></a>
-          <a id="instagram" target="_blank" href="https://www.instagram.com/soam_savonneriebio/"><img src="wp-content/themes/soam/images/icons/instagram.svg" alt="Logo Instagram"></a>
+          <a id="facebook" target="_blank" href="https://www.facebook.com/SoAmsavonneriebbio"  ><img src="<?php echo get_template_directory_uri() . '/images/icons/facebook.svg'; ?>" alt="Logo facebook"></a>
+          <a id="instagram" target="_blank" href="https://www.instagram.com/soam_savonneriebio/"><img src="<?php echo get_template_directory_uri() . '/images/icons/instagram.svg'; ?>" alt="Logo Instagram"></a>
         </div>
       </div>
     </header>
