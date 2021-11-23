6817e235d1eade7b2ce9748c51346e556f1621c5
Loriane <Loriane.Mace@etu.univ-savoie.fr>
2021-11-23 14:46:38 +0100

    Update front-page.php

--- www/wp-content/themes/soam/front-page.php
+++ www/wp-content/themes/soam/front-page.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /// Homepage
 
 get_header();
@@ -10,11 +10,11 @@ get_header();
             Artisant Savonnier responsable, local et respectueux de l’environnement !
         </h1>
         <p class="text-base">
-            Soâm est une savonnerie qui s’engage à respecter votre peau en utilisant des ingrédients 100% naturels. 
+            Soâm est une savonnerie qui s’engage à respecter votre peau en utilisant des ingrédients 100% naturels.
             Implanté en Savoie, dans le massif des Bauges, tous les savons et cosmétiques sont confectionnés
              à la main pour répondre au mieux aux besoins de chacun.
         </p>
-        <a class="button-xl bg-cyan color-white" href="">Viens me découvrir ! </a>
+        <a class="button-xl bg-cyan color-white" href=""><button>Viens me découvrir !</button> </a>
     </div>
     <img src="https://picsum.photos/488/515" alt="Image savons">
 </section>
@@ -67,14 +67,14 @@ get_header();
 </section>
 
 <!--
-    Besoin de conseils ? 
+    Besoin de conseils ?
 -->
 <section id="advice-section">
 
 </section>
 
 <!--
-points de ventes 
+points de ventes
 -->
 <section id="selling-points-section">
 
