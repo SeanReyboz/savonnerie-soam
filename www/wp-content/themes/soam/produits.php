fb547d227ec0d158ba4a722ed9903312e647f631
noeflp <Noe.Flipo@etu.univ-smb.fr>
2021-11-23 10:01:25 +0100

    Update produits.php
    html avancé

--- www/wp-content/themes/soam/produits.php
+++ www/wp-content/themes/soam/produits.php
@@ -5,11 +5,130 @@
     <title></title>
   </head>
   <body>
+    <div class="search_menu">
+      <a href="#"></a>
+    </div>
+    <div class="products">
 
+      <div class="soap">
+        <h2></h2>
+        <div class="soap_window">
+          <button type="button" name="neemteem">
+            <img src="" alt="Le savon Neem Teem">
+            <h3>Neem Teem</h3>
+          </button>
 
+          <button type="button" name="dendelion">
+            <img src="" alt="Le savon Dendelion">
+            <h3>Dendelion</h3>
+          </button>
+
+          <button type="button" name="sativa">
+            <img src="" alt="Le savon Sativa">
+            <h3>Sativa</h3>
+          </button>
+
+          <button type="button" name="yemanjah">
+            <img src="" alt="Le savon Yemanjah">
+            <h3>Yemanjah</h3>
+          </button>
+
+          <button type="button" name="rebelle">
+            <img src="" alt="Le savon le Rebelle">
+            <h3>Le Rebelle</h3>
+          </button>
+
+          <button type="button" name="kaoline">
+            <img src="" alt="Le savon Kaoline">
+            <h3>Kaoline</h3>
+          </button>
+
+        </div>
+
+      </div>
+
+      <div class="oil">
+        <h2></h2>
+        <div class="oil_window">
+          <button type="button" name="bonnemine">
+            <img src="" alt="Huile sèche bonne mine">
+            <h3>Huile sèche bonne mine anti-âge</h3>
+          </button>
+
+          <button type="button" name="protectionsolaire">
+            <img src="" alt="Huile sèche protection solaire">
+            <h3>Huile sèche protection solaire</h3>
+          </button>
+
+          <button type="button" name="apressoleil">
+            <img src="" alt="Huile apaisante après-soleil">
+            <h3>Huile apaisante après-soleil</h3>
+          </button>
+
+          <button type="button" name="sublimante">
+            <img src="" alt="Huile sèche sublimante">
+            <h3>Huile sèche sublimante</h3>
+          </button>
+
+          <button type="button" name="coutouryeux">
+            <img src="" alt="Sérum les 3C coutours des yeux">
+            <h3>Sérum "les 3C" Coutours des yeux</h3>
+          </button>
+
+          <button type="button" name="postepilation">
+            <img src="" alt="Huile sèche post-épilation">
+            <h3>Huile sèche post-épilation</h3>
+          </button>
+
+          <button type="button" name="vergetureenceinte">
+            <img src="" alt="Huile de massage anti-vergeture (femme enceinte)">
+            <h3>Huile de massage anti-vergeture (femme enceinte)</h3>
+          </button>
+
+          <button type="button" name="soincheveuxsecs">
+            <img src="" alt="Huile soin cheveux secs">
+            <h3>Huile de soins cheveux secs</h3>
+          </button>
+
+          <button type="button" name="barbeoil">
+            <img src="" alt="Barb'Oil soin de la barbe">
+            <h3>Barb'Oil soin de la barbe</h3>
+          </button>
+
+          <button type="button" name="sportif">
+            <img src="" alt="Huile des sportifs avant et après">
+            <h3>Huile des sportifs avant et après</h3>
+          </button>
+
+          <button type="button" name="peauacnéique">
+            <img src="" alt="Huile apaisante et hydratante peau acnéique">
+            <h3>Huile apaisante et hydratante peau acnéique</h3>
+          </button>
+
+          <button type="button" name="vergeture">
+            <img src="" alt="Huile de massage anti-vergeture corps">
+            <h3>Huile de massage anti-vergeture corps</h3>
+          </button>
+
+        </div>
+
+      </div>
+
+      <div class="balm">
+        <h2></h2>
+
+      </div>
+
+    </div>
+    <div class="infos">
+      <div class="about">
+
+      </div>
+      <div class="find_me">
+
+      </div>
+    </div>
     <footer>
-
-
     </footer>
   </body>
 </html>
