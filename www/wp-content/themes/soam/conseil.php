dde585e37bf42e75755ae5bef83813f75e33a5d9
EloRun9 <Elodie.Heurtin@etu.univ-savoie.fr>
2021-11-23 13:28:28 +0100

    Create conseil.php

---
+++ www/wp-content/themes/soam/conseil.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <div class="tip1">
+          <div class="image1">
+              <img src="" alt="">
+            </div>
+          <div class="text1">
+              <h1>Soin des Cheveux</h1>
+              <img src="" alt="line">
+              <p></p>
+              <p></p>
+            </div>
+         </div>
+        
