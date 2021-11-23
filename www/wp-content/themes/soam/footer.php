1ff618502ad0b1a5836b618194d3874699a7280d
SeanReyboz <seanreyboz@protonmail.com>
2021-11-23 13:42:42 +0100

    Added basic brand info

--- www/wp-content/themes/soam/footer.php
+++ www/wp-content/themes/soam/footer.php
@@ -2,7 +2,7 @@
     <div class="content main-wrapper">
       <div class="brand-information">
         <img class="brand-logo" src="<?php echo get_template_directory_uri() . '/images/logo_white.png'  ?>" alt="Logo Soâm">
-        <p class="text-sm brand-desc">
+        <p class="text-sm color-white brand-desc">
           Une savonnerie artisanale et organique située dans les Bauges qui prend
           soin de votre corps et de la nature. Tous mes savons et cosmétiques sont
           100% locaux et faits mains avec des produits organiques de qualité.
