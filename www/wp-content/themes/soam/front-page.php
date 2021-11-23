d15f47355076ff2f6265769591495201cc28ed36
Léo Achard <leo.achard@hotmail.fr>
2021-11-23 10:17:58 +0100

    Update header

--- www/wp-content/themes/soam/front-page.php
+++ www/wp-content/themes/soam/front-page.php
@@ -5,7 +5,18 @@ get_header();
 
 ?>
 <section id="hero-section">
-
+    <div id="hero-info">
+        <h1 class="text-xxl">
+            Artisant Savonnier responsable, local et respectueux de l’environnement !
+        </h1>
+        <p class="text-base">
+            Soâm est une savonnerie qui s’engage à respecter votre peau en utilisant des ingrédients 100% naturels. 
+            Implanté en Savoie, dans le massif des Bauges, tous les savons et cosmétiques sont confectionnés
+             à la main pour répondre au mieux aux besoins de chacun.
+        </p>
+        <a href="">Viens me découvrir ! </a>
+    </div>
+    <img src="https://picsum.photos/488/515" alt="Image savons">
 </section>
 <section id="features-section">
 
