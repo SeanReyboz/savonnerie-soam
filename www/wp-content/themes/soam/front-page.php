df77fecf4f2038d8ef7149859757155df8f07085
SeanReyboz <seanreyboz@protonmail.com>
2021-11-23 09:27:09 +0100

    Added frontpage.php

---
+++ www/wp-content/themes/soam/front-page.php
@@ -0,0 +1,26 @@
+<?php 
+/// Homepage
+
+get_header();
+
+?>
+<section id="hero-section">
+
+</section>
+<section id="features-section">
+
+</section>
+<section id="about-section">
+
+</section>
+<section id="advice-section">
+
+</section>
+<section id="selling-points-section">
+
+</section>
+<?php
+
+get_footer();
+
+?>
