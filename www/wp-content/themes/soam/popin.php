2ffe0ffc8311ff53df7fbc9ca0ec3681e54bd4a6
noeflp <Noe.Flipo@etu.univ-smb.fr>
2021-11-23 10:21:16 +0100

    Update popin.php

--- www/wp-content/themes/soam/popin.php
+++ www/wp-content/themes/soam/popin.php
@@ -5,6 +5,26 @@
     <title></title>
   </head>
   <body>
+    <h2></h2>
+    <div class="product_presentation">
+      <div class="image_product">
+        <img src="" alt="le produit">
+      </div>
+
+      <div class="about_product">
+        <h3 class="product_effect"></h3>
+        <h4>Les actifs : </h4>
+        <div class="">
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+
+
+      </div>
+    </div>
 
   </body>
 </html>
