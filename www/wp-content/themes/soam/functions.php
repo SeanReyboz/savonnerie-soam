a1bb1807341325c2906a29821c1386fba3c299ef
SeanReyboz <seanreyboz@protonmail.com>
2021-11-24 17:05:41 +0100

    Added acf-extend + modified footer & selling points

--- www/wp-content/themes/soam/functions.php
+++ www/wp-content/themes/soam/functions.php
@@ -2,9 +2,15 @@
 function add_style() {
     //wp_enqueue_style('main-style', get_template_directory_uri() . '/style.css', false);
     wp_enqueue_style('main-css', get_template_directory_uri() . '/css/main.css?t='.time(), false);
-  }
-  add_action( 'wp_enqueue_scripts', 'add_style' );
+}
+add_action( 'wp_enqueue_scripts', 'add_style' );
 
+
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page();
+	
+}
 
 function debug($val){
   echo "<pre> ";
