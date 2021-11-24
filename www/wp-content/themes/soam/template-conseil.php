289b4de3786ed0ada9947d92ab437f65a944d9e1
EloRun9 <Elodie.Heurtin@etu.univ-savoie.fr>
2021-11-24 11:03:06 +0100

    Update template-conseil.php

--- www/wp-content/themes/soam/template-conseil.php
+++ www/wp-content/themes/soam/template-conseil.php
@@ -5,27 +5,25 @@ Template Name: Page conseils
  */
 get_header();
 ?>
-    <div class="tip1">
-      <div class="image1">
+    <div class="hair_care main-wrapper">
+      <div class="hair_image">
         <img src=<?php echo get_template_directory_uri().'/images/conseils/hair.png';?>  alt="Cheveux"/>
       </div>
-      <div class="text1">
-        <h1>Soin des Cheveux</h1>
-        <img src="" alt="line">
-        <p></p>
-        <p></p>
+      <div class="hair_text">
+        <h2 class="underlined_header text-xxl">Soin des Cheveux</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. At malesuada quis non consectetur hendrerit pellentesque hac. Elementum orci et egestas ut sit amet, enim est. Massa sed porta viverra sed tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. At malesuada quis non consectetur hendrerit pellentesque hac. Elementum orci et egestas ut sit amet, enim est. Massa sed porta viverra sed tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. At malesuada quis non consectetur hendrerit pellentesque hac. Elementum orci et egestas ut sit amet, enim est. Massa sed porta viverra sed tincidunt.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. At malesuada quis non consectetur hendrerit pellentesque hac. Elementum orci et egestas ut sit amet, enim est. Massa sed porta viverra sed tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. At malesuada quis non consectetur hendrerit pellentesque hac. Elementum orci et egestas ut sit amet, enim est. Massa sed porta viverra sed tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. At malesuada quis non consectetur hendrerit pellentesque hac. Elementum orci et egestas ut sit amet, enim est. </p>
       </div>
     </div>
 
-    <div class="tip2">
-      <div class="text2">
-        <h1>Soin de la peau</h1>
-        <img src="" alt="line">
-        <p></p>
-        <p></p>
+    <div class="skin_care main-wrapper">
+      <div class="skin_text">
+        <h2 class="underlined_header text-xxl">Soin de la peau</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. At malesuada quis non consectetur hendrerit pellentesque hac. Elementum orci et egestas ut sit amet, enim est. Massa sed porta viverra sed tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. At malesuada quis non consectetur hendrerit pellentesque hac. Elementum orci et egestas ut sit amet, enim est. Massa sed porta viverra sed tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. At malesuada quis non consectetur hendrerit pellentesque hac. Elementum orci et egestas ut sit amet, enim est. Massa sed porta viverra sed tincidunt.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. At malesuada quis non consectetur hendrerit pellentesque hac. Elementum orci et egestas ut sit amet, enim est. Massa sed porta viverra sed tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. At malesuada quis non consectetur hendrerit pellentesque hac. Elementum orci et egestas ut sit amet, enim est. Massa sed porta viverra sed tincidunt.Lorem ipsum dolor sit amet, consectetur adipiscing elit. At malesuada quis non consectetur hendrerit pellentesque hac. Elementum orci et egestas ut sit amet, enim est. Massa sed porta viverra sed tincidunt.</p>
       </div>
-      <div class="image2">
-        <img src="" alt="">
+      <div class="skin_image">
+      <img src=<?php echo get_template_directory_uri().'/images/conseils/hair.png';?>  alt="Cheveux"/>
       </div>
     </div>
 
@@ -40,6 +38,22 @@ get_header();
         <p>Name</p>
         <p>Email</p>
         <p>Message</p>
+      </div>
+    </div>
+
+    <div class="infos">
+      <div class="about">
+        <h2 class="text-xxl">A propos</h2>
+        <p class="about_text text-base">Soâm est une savonnerie qui s’engage à respecter votre peau en utilisant des ingrédients 100% naturels. Implanté en Savoie, dans le massif des Bauges, tous les savons et cosmétiques sont confectionnés à la main pour répondre au mieux aux besoins de chacun.</p>
+        <a class="button bg-magenta color-white" name="more">En savoir plus</a>
+      </div>
+
+      <div class="find_me">
+        <h2 class="text-xxl">Où me trouver ?</h2>
+        <p class="about_text text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In integer eu porta fusce vitae, nunc nunc turpis sagittis. Sit pulvinar placerat posuere tincidunt at pulvinar proin facilisis tortor. Viverra in erat sed pharetra odio vulputate ut malesuada eget. Curabitur donec.</p>
+        <a href="#adress">1177 Route des Croës,73340 Lescheraines</a>
+        <a href="#phone">0651600314</a>
+        <a href="#mail">Email@mail.com</a>
       </div>
     </div>
 
