12ab2e52638dcb290d020d520c4d0ed31af9829b
noeflp <Noe.Flipo@etu.univ-smb.fr>
2021-11-24 13:43:19 +0100

    update page404 with scss

--- www/wp-content/themes/soam/404.php
+++ www/wp-content/themes/soam/404.php
@@ -6,14 +6,14 @@ get_header();
 ?>
   <body>
     <div id="page404">
-      <h1 class="ft-nunito">Oups, erreur 404</h1>
-      <p>Il semblerait que la page ne soit plus disponible...</p>
-      <a id="button404" class="button-xl color-white bg-cyan mg-16px-t">Retour à l'accueil</a>
-    </div>
+      <h1 class="ft-nunito mg-136px-t">Oups, erreur 404</h1>
+      <p class="mg-40px-t">Il semblerait que la page ne soit plus disponible...</p>
+      <a id="button404" class="button-xl color-white bg-cyan mg-40px-t">Retour à l'accueil</a>
 
-    <img src="" alt="blueblot">
-    <img src="" alt="purpleblot">
-    <img src="" alt="yellowblot">
+      <img id="page404-blue"src="<?php echo get_template_directory_uri() . "/images/blot/blueblot.png"; ?>" alt="blueblot">
+      <img id="page404-purple"src="<?php echo get_template_directory_uri() . "/images/blot/purpleblot.png"; ?>" alt="purpleblot">
+      <img id="page404-yellow"src="<?php echo get_template_directory_uri() . "/images/blot/yellowblot.png"; ?>" alt="yellowblot">
+    </div>
 
   </body>
 </html>
