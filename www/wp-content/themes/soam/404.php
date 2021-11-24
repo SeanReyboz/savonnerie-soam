e49d9c940a2b364017d0f6ab5b1060822b8f19ff
noeflp <Noe.Flipo@etu.univ-smb.fr>
2021-11-24 09:05:49 +0100

    created 404 and update main

---
+++ www/wp-content/themes/soam/404.php
@@ -0,0 +1,19 @@
+<?php
+/// Homepage
+
+get_header();
+
+?>
+  <body>
+    <div class="text">
+      <h1>Oups, erreur 404</h1>
+      <p>Il semblerait que la page ne soit plus disponible...</p>
+      <button type="button" value="Retour à l'accueil" name="home_page"></button>
+    </div>
+
+    <img src="" alt="">
+    <img src="" alt="">
+    <img src="" alt="">
+
+  </body>
+</html>
