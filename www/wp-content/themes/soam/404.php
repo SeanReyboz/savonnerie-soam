5099b90dfb49452a2e9e24dcf7dc726f51ef44aa
noeflp <Noe.Flipo@etu.univ-smb.fr>
2021-11-25 21:56:17 +0100

    Update 404

--- www/wp-content/themes/soam/404.php
+++ www/wp-content/themes/soam/404.php
@@ -9,10 +9,9 @@
 
   <body>
     <div id="page404">
-      <h1 class="ft-nunito mg-136px-t">Oups, erreur 404</h1>
-      <p class="mg-40px-t">Il semblerait que la page ne soit plus disponible...</p>
-      <a id="button404" href="<?php echo home_url(); ?>" class="button-xl color-white bg-cyan mg-40px-t">Retour à l'accueil</a>
-
+        <h1 class="ft-nunito mg-136px-t">Oups, erreur 404</h1>
+        <p class="mg-40px-t">Il semblerait que la page ne soit plus disponible...</p>
+        <a id="button404" href="<?php echo home_url(); ?>" class="button-xl color-white bg-cyan mg-40px-t">Retour à l'accueil</a>
       <img id="page404-blue"src="<?php echo get_template_directory_uri() . "/images/blot/blueblot.png"; ?>" alt="blueblot">
       <img id="page404-purple"src="<?php echo get_template_directory_uri() . "/images/blot/purpleblot.png"; ?>" alt="purpleblot">
       <img id="page404-yellow"src="<?php echo get_template_directory_uri() . "/images/blot/yellowblot.png"; ?>" alt="yellowblot">
