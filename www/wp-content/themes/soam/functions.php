c10f288ac5bb15f703f8f60594ef41bf74610139
SeanReyboz <seanreyboz@protonmail.com>
2022-01-04 21:36:48 +0100

    Added custom menu

--- www/wp-content/themes/soam/functions.php
+++ www/wp-content/themes/soam/functions.php
@@ -1,4 +1,22 @@
 <?php
+
+
+
+
+if (!function_exists('custom_register_nav_menu')) {
+    function custom_register_nav_menu() {
+        register_nav_menus([
+            'primary_menu' => 'Mon Menu principal',
+        ]);
+    }
+    add_action('after_setup_theme', 'custom_register_nav_menu', 0);
+}
+
+
+
+
+
+
 function add_style() {
     //wp_enqueue_style('main-style', get_template_directory_uri() . '/style.css', false);
     wp_enqueue_style('main', get_template_directory_uri() . '/css/main.css?t='.time(), false);
--- www/wp-content/themes/soam/header.php
+++ www/wp-content/themes/soam/header.php
@@ -6,23 +6,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
     <title><?php bloginfo('name'); ?></title>
-    <?php wp_head(); ?>
+    <?php 
+      wp_head(); 
+      show_admin_bar(true); 
+    ?>
   </head>
   <body>
     <header>
       <div class="main_nav flex-row">
-        <a href=""><img class="logo" src="<?php echo get_template_directory_uri() . '/images/logo.png'; ?>" alt="Logo Soam"/></a>
+        <a 
+          href="<?php  
+            /**
+             * @BUG
+             * La fonction `get_home_path()` fait planter toute la page pour une
+             * raison totalement inconnue...
+             * 
+             * @TODO
+             * Trouver une solution ou une alternative...
+             */
+
+            // echo get_home_path(); 
+          ?>"
+        >
+          <img 
+            class="logo" 
+            src="<?php echo get_template_directory_uri() . '/images/logo.png'; ?>" 
+            alt="Logo Soam" 
+          />
+        </a>
+        <?php
         
-        <ul class="nav flex-row ">
-          <li><a class="text" href="">Gamme Soam</a></li>
-          <li><a class="text" href="">À propos</a></li>
-          <li><a class="text"  href="">Conseils pratiques</a></li>
-          <li><a class="text"  href="">Partenaires</a></li>
-          <li><a class="text"  href="">Points de ventes</a></li>
-        </ul>
+        // Afficher le menu
+          wp_nav_menu([
+            'menu' => 'primary_menu',
+            'container' => 'nav',
+            'menu_class' => 'nav flex-row',
+          ]); 
+
+        ?>
+
         <div class="rs_icons">
-          <a id="facebook" class="mg-16px-r" target="_blank" href="<?php echo get_field('instagram','option'); ?>" alt="Logo facebook"></a>
-          <a id="instagram" target="_blank" href="<?php echo get_field('facebook','option'); ?>"><img src="" alt="Logo Instagram"></a>
+          <a 
+            id="facebook" 
+            class="mg-16px-r" 
+            target="_blank"
+            rel="noopener noreferrer"
+            href="<?php echo get_field('instagram', 'option'); ?>" 
+            alt="Logo facebook"
+          >
+            <img 
+              src="<?php echo get_template_directory_uri() . '/images/icons/facebook.svg'; ?>"
+              alt="Icone Facebook" 
+            />
+          </a>
+          <a 
+            id="instagram" 
+            target="_blank"
+            rel="noopener noreferrer"
+            href="<?php echo get_field('facebook', 'option'); ?>"
+          >
+            <img 
+              src="<?php echo get_template_directory_uri() . '/images/icons/instagram.svg'; ?>" 
+              alt="Logo Instagram" 
+            />
+          </a>
         </div>
       </div>
     </header>
