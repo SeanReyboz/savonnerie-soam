2337280d531afda80b079526602e10bf080e83f5
SeanReyboz <seanreyboz@protonmail.com>
2022-03-03 21:54:45 +0100

    Dernier commit avant dodo :sleeping:

--- www/wp-content/themes/soam/template-conseil.php
+++ www/wp-content/themes/soam/template-conseil.php
@@ -2,6 +2,11 @@
 <?php
 /*
 Template Name: Page conseils
+
+
+TODO: Refactor
+TODO: Vérifier + faire la version responsive
+TODO: Vérifier dynamisation
  */
 get_header();
 
@@ -68,11 +73,11 @@ $soins = get_field('soins');
   ?>
     <div class="contact">
       <div class="formulaire_image">
-      <img class="form_image" src="<?= get_template_directory_uri().'/images/conseils/contacter.png' ?>" alt="contact_image"/>
+        <img class="form_image" src="<?= get_template_directory_uri().'/images/conseils/contacter.png' ?>" alt="contact_image"/>
       </div>
       <div class="formulaire">
         <h2 class="contact-me text-xxl ft-nunito ft-bold">Me contacter</h2>
-        <p class="desc text-base">
+        <p class="desc text-base mg-32px-y">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. At malesuada quis non consectetur hendrerit
             pellentesque hac. Elementum orci et egestas ut sit amet, enim est. Massa sed porta viverra sed
             tincidunt.
@@ -97,7 +102,6 @@ $soins = get_field('soins');
 
     <!-- Bloc d'infos complémentaires -->
     <?php include "pre-footer-info-block.php"; ?>
-
 
 <?php
 
