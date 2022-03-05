a851eab8fa139577524c57132d39b7f5ad129136
Emmanuel <90639619+Swartyc@users.noreply.github.com>
2022-03-06 00:36:35 +0100

    add "a propos" and "partenaires" pages with images

---
+++ www/wp-content/themes/soam/page-a-propos.php
@@ -0,0 +1,13 @@
+<?php
+get_header();
+?>
+
+<!-- Bloc de contenu princpale -->
+<?php include "contenu-principaux.php"; ?>
+
+<!-- Bloc d'infos complémentaires -->
+<?php include "pre-footer-info-block.php"; ?>
+
+<?php
+get_footer();
+?>
