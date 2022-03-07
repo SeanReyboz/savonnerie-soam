76555c55df112f856fb81eb951e4f42c2293a249
SeanReyboz <seanreyboz@protonmail.com>
2022-03-07 21:28:54 +0100

    Content page layout refactor

--- www/wp-content/themes/soam/contenu-principaux.php
+++ www/wp-content/themes/soam/contenu-principaux.php
@@ -1,62 +1,26 @@
 <?php
 $contenu_en_quinconce = get_field('contenu_en_quinconce');
+
+//debug($contenu_en_quinconce);
 ?>
 
 <?php
-    if(count($contenu_en_quinconce)==1):
-      foreach ( $contenu_en_quinconce as $key => $content ):
-  ?>
-    <div class="content main-wrapper">
-      <div class="content_image">
-        <img src="<?php echo $content['image']['url'] ?>" alt="<?php echo $content['image']['alt']  ?>">
-      </div>
-      <div class="content_text">
-        <h2 class="underlined_header text-xxl ft-nunito ft-bold"><?php echo $content['titre_du_contenu']; ?></h2>
-        <div><?php echo $content['contenu']; ?></div>
-      </div>
-    </div>
-  <?php
-      endforeach;
-    endif;
-  ?>
+    foreach ( $contenu_en_quinconce as $key => $content ) :
 
-  <?php
-    if(count($contenu_en_quinconce)>1):
-      foreach ( $contenu_en_quinconce as $key => $content ):
-  ?>
-    <?php
-      if(!($key&1)):
-    ?>
-    <div class="content main-wrapper">
-      <div class="content_image">
-        <img src="<?php echo $content['image']['url'] ?>" alt="<?php echo $content['image']['alt']  ?>">
-      </div>
-      <div class="content_text">
-        <h2 class="underlined_header text-xxl ft-nunito ft-bold"><?php echo $content['titre_du_contenu']; ?></h2>
-        <div class="text-base"><?php echo $content['contenu']; ?></div>
-      </div>
+        $even_or_odd = $key % 2 === 0 ? "even" : "odd";
+?>
+    <div
+        class="content main-wrapper"
+	    data-is-<?= $even_or_odd ?>
+    >
+        <div class="content_image">
+            <img src="<?= $content['image']['url'] ?>" alt="<?= $content['image']['alt']  ?>">
+        </div>
+        <div class="content_text">
+            <h2 class="underlined_header text-xxl ft-nunito ft-bold"><?= $content['titre_du_contenu']; ?></h2>
+            <div class="text-base"><?= $content['contenu']; ?></div>
+        </div>
     </div>
-    <?php
-      endif;
-    ?>
-
-    <?php
-      if($key&1):
-    ?>
-    <div class="content main-wrapper">
-      <div class="content_text">
-        <h2 class="underlined_header text-xxl ft-nunito ft-bold"><?php echo $content['titre_du_contenu']; ?></h2>
-        <div class="text-base"><?php echo $content['contenu']; ?></div>
-      </div>
-      <div class="content_image">
-        <img src="<?php echo $content['image']['url'] ?>" alt="<?php echo $content['image']['alt']  ?>">
-      </div>
-    </div>
-    <?php
-      endif;
-    ?>
-
-  <?php
-      endforeach;
-    endif;
-  ?>
+<?php
+    endforeach;
+?>
